cd65b5f8c1eafe2eebbcea644166d672868ef5df
Aman Kumar <71450621+Kumar-Amaan@users.noreply.github.com>
2021-04-28 15:35:09 +0530

    Project Files

---
+++ src/components/pages/Products.js
@@ -0,0 +1,18 @@
+// import React from 'react'
+// import '../../App.css'
+
+// function Product() {
+//     return (
+//         <>
+//             <h1 className="products">Products</h1>
+//         </>
+//     )
+// }
+
+// export default Product
+import React from "react";
+import "../../App.css";
+
+export default function Products() {
+  return <h1 className="products">PRODUCTS</h1>;
+}
